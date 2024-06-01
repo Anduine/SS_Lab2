b35c269547a0f05e9c58a967223731588b59724d
Anduine <35.artem.lern@gmail.com>
2024-06-01 16:38:38 +0300

    Func 2

--- SS_Lab2/gtrToxml.cpp
+++ SS_Lab2/gtrToxml.cpp
@@ -1,42 +1,70 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <stack>
 #include "gtrToxml.h"
 
-void parseGTR(std::string inputfile, std::string outfile)
+void lowercase(std::string &str)
+{
+	for (int i = 0; i < str.length(); i++)
+	{
+		str[i] = tolower(str[i]);
+	}
+}
+
+void parseGTR(std::string nameinputfile, std::string nameoutfile)
 {
 	std::string buff;
-	std::ifstream inputf(inputfile);
+	std::stack<int> levels;
+	bool closepers = false;
+	std::ifstream inputf(nameinputfile);
 	if (!inputf) {
 		std::cerr << "Unable to open input file\n";
 		return;
 	}
-	std::ofstream outputf(outfile);
+	std::ofstream outputf(nameoutfile);
 	if (!outputf) {
 		std::cerr << "Unable to open output file\n";
 		return;
 	}
 
+	char g;
+	int counter{ 1 };
+
 	while (inputf)
 	{
-		inputf >> buff;
-		if (buff == "1")
+		inputf >> g;
+		if (g-48 != counter)
 		{
+			levels.push(counter);
+
+		}
+		if (g-48 > counter)
+		{
+			outputf << '\t';
+			counter++;
+		}
+
 			inputf >> buff;
 			if (buff == "NAME")
 			{
 				inputf >> buff;
-				outputf << "<person NAME=\"" << buff << "\" ";
+				outputf << "<person name=\"" << buff << "\" ";
+			}
+			else if (buff == "BIRTHDATE")
+			{
+				lowercase(buff);
+				outputf << buff;
+				inputf >> buff;
+				outputf << "=\"" << buff << "\">\n";
 			}
 			else
 			{
+				lowercase(buff);
 				outputf << buff;
 				inputf >> buff; 
 				outputf << "=\"" << buff << "\" ";
 			}
-
-		}
-
 	}
 
 	inputf.close();
